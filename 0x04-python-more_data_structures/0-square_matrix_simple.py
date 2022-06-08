3a55b1e178a16af6c1d3b6774463f6616da785b6
nati <nati@gmail.com>
2022-06-08 08:26:37 -0700

    squire matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-
-if number > 0:
-    print("{:d} is positive".format(number))
-elif number < 0:
-    print("{:d} is negative".format(number))
-else:
-    print("{:d} is zero".format(number))
+def square_matrix_simple(matrix=[]):
+    return [list(map((lambda x: x ** 2), i)) for i in matrix]
